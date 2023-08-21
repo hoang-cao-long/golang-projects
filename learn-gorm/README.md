dd7cc14a4fd4e5fe40e153ed7b10d9666b69a8fc
hoang-cao-long <hclong2k@gmail.com>
2023-08-21 22:23:37 +0700

    add monorepo concept

--- learn-gorm/README.md
+++ learn-gorm/README.md
@@ -1,3 +1,4 @@
 # Learn
     - Connect, migrate, query with gorm
     - Read config file with viper
+    - Monorepo concept
---
+++ learn-gorm/monorepo/services/core/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	u "github.com/hoang-cao-long/learn-gorm/monorepo/services/gateway/utils"
+)
+
+func main() {
+	fmt.Println(u.ChaoBan())
+}
