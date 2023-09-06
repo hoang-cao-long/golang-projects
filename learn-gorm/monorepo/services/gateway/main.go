b7cdc2f377872385b85dcf466a268373a8a96624
hoang-cao-long <hclong2k@gmail.com>
2023-09-06 22:05:21 +0700

    chore

--- learn-gorm/monorepo/services/gateway/main.go
+++ learn-gorm/monorepo/services/gateway/main.go
@@ -1,5 +1,23 @@
+// You can edit this code!
+// Click here and start typing.
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+type test struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	// a := test{
+	// 	Name: "long",
+	// 	Age:  18,
+	// }
+	b := reflect.TypeOf(test{})
+
+	fmt.Println(b.FieldByIndex([]int{0, 1}))
 }
